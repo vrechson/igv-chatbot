f31df928db97452d88b7f7363961e4474cb57683
Matheus Vrech <whoismath@gmail.com>
2018-03-14 00:11:10 -0300

    aaaaaa

--- app/commands/rand.js
+++ app/commands/rand.js
@@ -4,11 +4,11 @@ module.exports = async (msg, bot, n, service) => {
   if (n <= 0)
     n = 3
 
-  for (let i, const data = service.listIds(); i < n; i++) {
-    while (data[i = rand() % data.length] == "invalid") {}
+//  for (let i, const data = service.listIds(); i < n; i++) {
+  //  while (data[i = rand() % data.length] == "invalid") {}
 
     //
 
-    data[i] = "invalid"
-  }
+  //  data[i] = "invalid"
+//  }
 }
