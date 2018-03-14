157ef32cdddd5727172cf2c0b8c9d8213b0d04e4
Matheus Vrech <whoismath@gmail.com>
2018-03-14 00:10:16 -0300

    odeio javascript

--- app/commands/last.js
+++ app/commands/last.js
@@ -4,8 +4,8 @@ module.exports = async (msg, bot, n, service) => {
   if (n <= 0)
     n = 3
 
-  for (let i = n - 1, const data = service.listIds(); i > 0; i--) {
+  //for (let i = n - 1, const data = service.listIds(); i > 0; i--) {
     //
-  }
+//  }
 
 }
