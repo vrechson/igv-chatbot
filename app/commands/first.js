d84400603ea9a262d076d871dde26c7538216cf9
Matheus Vrech <whoismath@gmail.com>
2018-03-14 00:08:34 -0300

    aff

--- app/commands/first.js
+++ app/commands/first.js
@@ -4,7 +4,7 @@ module.exports = async (msg, bot, n, service) => {
   if (n <= 0)
     n = 3
 
-  for (let i = 0, const data = service.listIds(); i < n; i++) {
+//  for (let i = 0, const data = service.listIds(); i < n; i++) {
     //
-  }
+  //}
 }
